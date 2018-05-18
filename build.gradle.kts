fe050ab56ae9c1eca0abadaa66cfc21fff24c200
Fabian Mächler <fabian.maechler@noser.com>
2018-05-18 10:52:38 +0200

    Refactored using com.zyxist.chainsaw Plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,40 @@
 group = "com.noser"
 version = "1.0-SNAPSHOT"
 
+val moduleName by extra { "com.noser.hello" }
+
+extra["moduleName"] = "com.noser.hello"
+
+
+buildscript {
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+}
+
 allprojects {
     apply(plugin = "java")
+    apply(plugin = "com.zyxist.chainsaw")
+
     configure<JavaPluginConvention> {
         sourceCompatibility = JavaVersion.VERSION_1_10
         targetCompatibility = JavaVersion.VERSION_1_10
     }
 }
 
-subprojects {
-    val moduleName: String by this.extra
-    afterEvaluate {
-        tasks.withType<JavaCompile> {
-            inputs.property("moduleName", moduleName)
-            doFirst {
-                logger.error("wiiii module $moduleName")
-                options.compilerArgs.addAll(arrayOf("--module-path", classpath.asPath))
-            }
-        }
-    }
+repositories {
+    mavenCentral()
+    jcenter()
 }
 
 plugins {
     application
+    id("com.zyxist.chainsaw") version "0.3.1"
 }
 
 application {
     mainClassName = "com.noser.hello.Main"
-}
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
